9642559321eb8f5eb42275b48d309afe8724bd35
robert <robertrenbysanjuan@gmail.com>
2025-07-10 06:15:06 +0800

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import {RouterProvider,
 
 import Layout from './components/layout'
 
+import Home from './components/pages/Home'
+
 export default function App(){
     const router = createBrowserRouter(createRoutesFromElements(
         <Route path='/' element={<Layout />}>
-    
+            <Route index element={<Home />}/>
         </Route>
     ))
 
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
+
 import { useEffect, useState } from "react";
 
 export default function Layout(){
