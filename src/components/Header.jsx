e0890756e96737760c218aa876a58e69828d0b27
robert <robertrenbysanjuan@gmail.com>
2025-07-09 14:06:10 +0800

    styled header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,60 @@
 import { NavLink } from "react-router-dom"
+import menuBurger from '../assets/menu.png'
+import { useEffect, useState } from "react"
 
-export default function Header(){
-    return(
-        <header>
-            <img src="/logo.png" alt="R.Cortez Realty" />
+export default function Header({ isMobile }){
 
-            <nav>
-                <NavLink to={'/'}>Home</NavLink>
-                <NavLink to={'/listings'}>Listings</NavLink>
-                <NavLink to={'/properties'}>Properties</NavLink>
-                <NavLink to={'/privacy-policy'}>Privacy Policy</NavLink>
-                <NavLink to={'/contact'}>Contact</NavLink>
+    const [showMenu, setShowMenu] = useState(false)
+
+    useEffect(()=>{
+
+        if(showMenu && !isMobile) setShowMenu(false)
+
+        
+    }, [isMobile])
+
+    const navigation = <>
+            <nav className="nav-window">
+                <NavLink className={({isActive}) => isActive ? 'navActive' : ''} to={'/'}>Home</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'navActive' : ''} to={'/listings'}>Listings</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'navActive' : ''} to={'/properties'}>Properties</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'navActive' : ''} to={'/privacy-policy'}>Privacy Policy</NavLink>
             </nav>
 
-            <NavLink to={'/login'}>Login</NavLink>
+            <div>
+                <NavLink to={'/contact'}>Contact</NavLink>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
+        </>
+
+    const mobileNavigation = <>
+        <button onClick={()=>{setShowMenu(true)}} className="menuBtn"><img src={menuBurger} alt="menu" /></button>
+
+        <div className={`nav-mobile-container ${showMenu ? 'show' : ''}`}>
+            <nav className="nav-mobile">
+                    <NavLink to={'/'}>Home</NavLink>
+                    <NavLink to={'/listings'}>Listings</NavLink>
+                    <NavLink to={'/properties'}>Properties</NavLink>
+                    <NavLink to={'/privacy-policy'}>Privacy Policy</NavLink>
+
+                    <div>
+                        <NavLink className={`contact contact-login`} to={'/contact'}>Contact</NavLink>
+                        <NavLink className={`login contact-login`} to={'/login'}>Login</NavLink>
+                    </div>
+            </nav>
+
+            <button onClick={() => {setShowMenu(false)}}>X</button>
+        </div>
+
+    </>
+
+
+    return(
+        <header>
+            <NavLink to={'/'}><img src="/logo.png" alt="R.Cortez Realty" /></NavLink>
+
+            {isMobile ? mobileNavigation : navigation}
+
         </header>
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,24 @@
 
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { useEffect, useState } from "react";
 
 export default function Layout(){
 
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
+
+    useEffect(()=>{
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768)
+        }
+
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
     return(
         <>
-            <Header />
+            <Header isMobile={isMobile}/>
             <Outlet />
         </>
     )
